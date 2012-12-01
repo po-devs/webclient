a3c48e6194e65e59aae57112cc79060116b34b11
EthanPO <ethanpokemononline@hotmail.com>
2012-11-30 20:40:43 -0600

    js -> utils

--- libs/utils.js
+++ libs/utils.js
@@ -1,5 +1,4 @@
 var utils = {
-    /* Stolen from here: http://www.bloggingdeveloper.com/post/JavaScript-QueryString-ParseGet-QueryString-with-Client-Side-JavaScript.aspx */
     getQuerystring: function(key, default_) {
       if (default_==null) default_=""; 
       key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
