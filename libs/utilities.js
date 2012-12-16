a72037016bbd79fae3197202585ff958b4c8c930
coyotte508 <coyotte508@hotmail.com>
2012-12-16 02:28:51 +0100

    Trainer info now shows, as well as avatar
    Fix #40, fix #42 and fix part of #10

--- libs/utilities.js
+++ libs/utilities.js
@@ -1,12 +1,6 @@
 function getQueryString(key, default_,query_) {
-    if (query_===undefined) query_=window.location.href;
-    key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-    var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");
-    var qs = regex.exec(query_);
-    if(qs == null)
-        return default_;
-    else
-        return qs[1];
+    if (query_===undefined) query_=window.location.search;
+    return decodeURI(window.location.search.replace(new RegExp("^(?:.*[&\\?]" + encodeURI(key).replace(/[\.\+\*]/g, "\\$&") + "(?:\\=([^&]*))?)?.*$", "i"), "$1"));
 }
 
 function loadjscssfile(filename, filetype){
