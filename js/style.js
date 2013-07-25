5b3e1b32b9f9681f8be6d60684b8c9b130b010ec
coyotte508 <coyotte508@hotmail.com>
2013-07-25 17:00:23 +0200

    more progress towards jira's design

--- js/style.js
+++ js/style.js
@@ -5,44 +5,19 @@ $(document).ready(function() {
 			return $(elem).text().toUpperCase().indexOf(arg.toUpperCase()) >= 0;
 		};
 	});
-	$("#tabs").sortable().on('sortstart', function(event, ui) {
-		$("#tabs span").each(function(index) {
-			$(this).attr('id', 'tab_temporary_id_'+index);
-			$(".chat_messages").eq(index).attr('id', 'chat_temporary_id_'+index)
-		});
-	}).on('sortupdate', function(event, ui) {
-		var chat_selector = "#chat_"+ui.item.attr('id').substr(4);
-		if(ui.item.index() > $(chat_selector).index())
-		{
-			$(".chat_messages").eq(ui.item.index()).after($(chat_selector));
-		}
-		else
-		{
-			$(".chat_messages").eq(ui.item.index()).before($(chat_selector));
-		}
-	}).on('sortstop', function(event, ui) {
-		$("#tabs span").removeAttr('id'),
-		$(".chat_messages").removeAttr('id');
-	});
 	
-	$("#tabs span").on('dblclick', function() {
-		$('.active_tab').removeClass('active_tab');
-		$(this).addClass('active_tab');
-		$(".chat_messages").css('display', 'none').eq($(this).index()).css('display', 'block');
-	});
-	
-	$("#tabs span i").on('click', function() {
-		if($("#tabs span").length > 1)
+	$("#tab-titles span i").on('click', function() {
+		if($("#tab-titles span").length > 1)
 		{
 			var position = $(this).parent().index();
 			
 			if($(this).parent().hasClass('active_tab'))
 			{
-				var offset = position+1 == $("#tabs span").length ? -1 : 1;
-				$("#tabs span").eq(position+offset).addClass('active_tab');
+				var offset = position+1 == $("#tab-titles span").length ? -1 : 1;
+				$("#tab-titles span").eq(position+offset).addClass('active_tab');
 				$(".chat_messages").eq(position+offset).css('display', 'block');
 			}
-			$("#tabs span").eq(position).remove();
+			$("#tab-titles span").eq(position).remove();
 			$(".chat_messages").eq(position).remove();
 		}
 	});
