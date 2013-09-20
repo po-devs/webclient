da44268d92ff6ce6752d99d99be4cf84bc0148a6
coyotte508 <coyotte508@gmail.com>
2013-09-20 05:26:47 +0200

    proper default sorting for players list

--- js/index.js
+++ js/index.js
@@ -530,6 +530,8 @@ parseCommand = function(message) {
                 + "<td>"+server.ip+":" + server.port + "</td></tr>";
             $("#servers-list tbody").prepend(html);
         }
+
+        $("#servers-list").tablesorter({sortList: [[1,1]]});
     } else if (cmd == "connected") {
         displayMessage("Connected to server!");
 
