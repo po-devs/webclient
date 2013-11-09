caa38c2430391b2bfb9e8f7edd37089eb4c6b4b0
Jirachier <jiraichier@gmail.com>
2013-11-09 16:21:31 +0000

    switching sprite folder of gen6 to x-ty

--- js/db/generations.options.js
+++ js/db/generations.options.js
@@ -100,6 +100,6 @@ pokedex.generations.options = {
 	item:true,
 	evs:true,
 	types_ids:[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
-	sprite_folder:'http://pokemon-online.eu/images/pokemon/black-white/'
+	sprite_folder:'http://pokemon-online.eu/images/pokemon/x-y/'
 }
 };
