86741fbd72e4e0a83c0282808b8535260acac7a3
TheUnknownOne <theunknownone25@gmail.com>
2013-11-14 21:15:12 +0100

    Fix tests

--- test/pokeinfo-test.js
+++ test/pokeinfo-test.js
@@ -8,8 +8,8 @@ buster.testCase("pokeinfo", {
         assert.equals("http://pokemon-online.eu/images/pokemon/x-y/1.png", pokeinfo.sprite({num: 1}));
     },
     "compact database": function () {
-        assert.equals(85, pokeinfo.accuracy(3, 1));
-        assert.equals(1, pokeinfo.damageClass(44, 3));
+        assert.equals(85, moveinfo.accuracy(3, 1));
+        assert.equals(1, moveinfo.damageClass(44, 3));
         assert.equals(2, pokeinfo.types(18, 4)[0]);
     }
 });
