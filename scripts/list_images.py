fad4f0a6eccda363f706e51954a2f2881fed4e51
coyotte508 <coyotte508@gmail.com>
2013-11-17 02:24:58 +0100

    Add back image infos

--- scripts/list_images.py
+++ scripts/list_images.py
@@ -12,47 +12,54 @@ def main(argv):
     else:
         path = argv[1]
 
-    os.system('find ' + path + '/* -prune -type f -name "*.png" -exec convert {} -print "{}:%w,%h\n" /dev/null \; > images.txt')
-    os.system('find ' + path + '/animated/* -prune -type f -name "*.gif" -exec convert {} -print "{}:%w,%h\n" /dev/null \; >> images.txt')
+    for folder in ('', '/back/'):
+        os.system('find ' + path + folder + '/* -prune -type f -name "*.png" -exec convert {} -print "{}:%w,%h\n" /dev/null \; > images.txt')
+        os.system('find ' + path + '/animated/' + folder + '/* -prune -type f -name "*.gif" -exec convert {} -print "{}:%w,%h\n" /dev/null \; >> images.txt')
 
-    lines = open("images.txt", "r").readlines()
-    images = {}
+        lines = open("images.txt", "r").readlines()
+        images = {}
 
-    for line in lines:
-        ls = line.strip().split(':')
-        num = ls[0].split('/')[-1].split('.')[0]
+        for line in lines:
+            ls = line.strip().split(':')
+            num = ls[0].split('/')[-1].split('.')[0]
 
-        if not num[0].isdigit():
-            continue
+            if not num[0].isdigit():
+                continue
 
-        while num[0] == '0' and len(num) > 1:
-            num = num[1:]
+            while num[0] == '0' and len(num) > 1:
+                num = num[1:]
 
-        if num.find('-') != -1:
-            num = num.split('-')
-            num = str(int(num[0]) + int(num[1]) * 65536)
+            if num.find('-') != -1:
+                num = num.split('-')
+                num = str(int(num[0]) + int(num[1]) * 65536)
 
-        wh = ls[1].split(',')
-        ext = ls[0][-3:]
+            wh = ls[1].split(',')
+            ext = ls[0][-3:]
 
-        if ext == 'gif':
-            images[num] = "{w:" + wh[0] + ",h:" + wh[1] + "}"
+            if ext == 'gif':
+                images[num] = "{w:" + wh[0] + ",h:" + wh[1] + "}"
+            else:
+                images[num] = "{ext:'" + ext + "',w:" + wh[0] + ",h:" + wh[1] + "}"
+
+        lines = []
+        for k,v in images.items():
+            line = k + ": " + v
+            print (line)
+            lines.append(line + ",\n")
+
+        lines.sort()
+        if len(folder) > 0:
+            type = "." + folder[1:-1]
         else:
-            images[num] = "{ext:'" + ext + "',w:" + wh[0] + ",h:" + wh[1] + "}"
-
-    lines = []
-    for k,v in images.items():
-        line = k + ": " + v
-        print (line)
-        lines.append(line + ",\n")
-
-    lines.sort()
-    output = open('../js/db/pokes/images.js', 'w')
-    output.write("if(!pokedex.pokes)pokedex.pokes={};\n")
-    output.write("pokedex.pokes.images")
-    output.write(" = {\n")
-    output.writelines(lines)
-    output.write("};")
+            type = ""
+        output = open('../js/db/pokes/images' + type + '.js', 'w')
+        output.write("if(!pokedex.pokes)pokedex.pokes={};\n")
+        if len(type) > 0:
+            output.write("if(!pokedex.pokes.images)pokedex.pokes.images={};\n")
+        output.write("pokedex.pokes.images"+type)
+        output.write(" = {\n")
+        output.writelines(lines)
+        output.write("};")
 
 if __name__ == "__main__":
     main(sys.argv)
