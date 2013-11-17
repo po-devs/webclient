474b89b8a9c2ed265c40a92fb88a874b1b3fdfe1
coyotte508 <coyotte508@gmail.com>
2013-11-17 23:02:09 +0100

    Trainer info fixed size for avatar
    So that the size is already there before the picture is loaded

--- js/channeltab.js
+++ js/channeltab.js
@@ -7,11 +7,11 @@ function ChannelTab(/* id */)
 ChannelTab.prototype.isCurrent = function()
 {
     return room == this;
-}
+};
 
 ChannelTab.prototype.activateTab = function()
 {
     if (!this.isCurrent()) {
         $("#channel-tabs > ul li a[href='#"+this.shortHand+"-"+this.id+"']").addClass("tab-active");
     }
-}
+};
