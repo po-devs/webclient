8858bafdf690febec7fe391e3aa331c1a31b2ec3
TheUnknownOne <theunknownone25@gmail.com>
2014-02-03 23:42:39 +0100

    Statinfo tests

--- test/loadpokedex.js
+++ test/loadpokedex.js
@@ -5,9 +5,7 @@ require('../js/db/initpokedex');
 require('../js/db/pokes/images');
 
 glob.sync("js/db/**/*.js", {dot: true}).forEach(function (f) {
-    if (f !== 'js/db/initpokedex.js' && f !== 'js/db/pokes/images.js') {
-        require('../' + f);
-    }
+    require('../' + f);
 });
 
 require('../js/db/generations');
